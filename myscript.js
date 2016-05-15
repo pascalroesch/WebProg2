9781703914ed081a48bf47728746d4efd3c66f2e
KnuckleJoe96 <luk_popp@yahoo.de>
2016-05-15 22:48:35 +0200

    Kleine Änderung beim Eingabefeld

--- myscript.js
+++ myscript.js
@@ -3,12 +3,24 @@ function init () {
 }
 
 function addCart () {
+    var element = document.getElementById("ideas");
+
+    var span = document.createElement("span");
+    span.classList.add("menupoint");
+
     var cart = document.createElement("input");
     cart.placeholder = "Hier etwas eingeben";
     cart.classList.add("cartInput");
 
-    var element = document.getElementById("ideas");
-    element.appendChild(cart);
+    var pen = document.createElement("img");
+    pen.src = "img/IconPen.png";
+    pen.height = 32;
+    pen.width = 32;
+
+    span.appendChild(cart);
+    span.appendChild(pen);
+    element.appendChild(span);
+
     
     // Toggle Button um zwischen Textfeld und statisch hin und her zu springen
     var toggleInput = document.createElement("div");
