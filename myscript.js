c6741b51fa6fcf9fc3949582a0c7c4bdb16b7b7a
KnuckleJoe96 <luk_popp@yahoo.de>
2016-05-17 00:55:55 +0200

    Menupoints can be toggled
    - TODO: vertically center the pen-button and the inputbox
    - Menupoints can be toggled on/off to switch between write and drag mode
    (when dragged they change opacity - not finished)

--- myscript.js
+++ myscript.js
@@ -1,12 +1,12 @@
 function init () {
-	console.log(1);
+
 }
 
 function addCart () {
     var element = document.getElementById("ideas");
 
-    var span = document.createElement("span");
-    span.classList.add("menupoint");
+    var menupoint = document.createElement("span");
+    menupoint.classList.add("menupoint");
 
     var cart = document.createElement("input");
     cart.placeholder = "Hier etwas eingeben";
@@ -14,17 +14,42 @@ function addCart () {
 
     var pen = document.createElement("img");
     pen.src = "img/IconPen.png";
-    pen.height = 32;
-    pen.width = 32;
+    pen.classList.add("pen");
 
-    span.appendChild(cart);
-    span.appendChild(pen);
-    element.appendChild(span);
+    menupoint.appendChild(cart);
+    menupoint.appendChild(pen);
+    element.appendChild(menupoint);
 
+    //toggleDraggable(menupoint);
+
+    addListeners(menupoint);
     
+    pen.addEventListener("click", toggleInput);
+
+    //Brauchen wir das hier noch? vvvvvvvvvv
     // Toggle Button um zwischen Textfeld und statisch hin und her zu springen
-    var toggleInput = document.createElement("div");
+    /*var toggleInput = document.createElement("div");
     toggleInput.classList.add("toggleInput");
-    element.appendChild(toggleInput);
-    
+    element.appendChild(toggleInput);*/
+}
+
+function toggleDraggable(menupoint){
+    menupoint.draggable = !(menupoint.draggable);
+}
+
+function dragStart(event){
+    this.style.opacity = '0.75';
+}
+
+function addListeners(menupoint){
+    menupoint.addEventListener("dragstart", dragStart);
+}
+
+function toggleInput(event){
+    var parent = this.parentNode;
+    toggleDraggable(parent);
+    var cart = parent.firstChild;
+
+    cart.disabled = !(cart.disabled); //readonly funktioniert irgendwie (noch) nicht; mit disabled möglich, aber nicht mehr schön, unschönes lösbar mit background-color&color
+    cart.classList.toggle("cartStyle");
 }
